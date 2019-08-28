baba2a7a34470c8ddec674f18721a8d7d0842dfd
rohit gupta <rohit.gupta@jellyfishtechnologies.com>
2019-08-28 17:52:40 +0530

    Change UI and implemented the image picker code on the step first

--- BundleApp/Controller/CreatePost/UploadID_StepFirstVC.swift
+++ BundleApp/Controller/CreatePost/UploadID_StepFirstVC.swift
@@ -8,20 +8,48 @@
 
 import UIKit
 
-class UploadID_StepFirstVC: UIViewController {
+class UploadID_StepFirstVC: UIViewController, SelectedImage {
 
+    @IBOutlet weak var proof_Image: UIImageView!
+    
+    var imagePicker : ImagePiker!
+    var imageData : NSData!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
         self.setRightBarButtonItems(Step: "01")
         self.setBackButtonWithTitle(title: "Create")
         // Do any additional setup after loading the view.
+        let tapUploadImage = UITapGestureRecognizer(target: self, action: #selector(self.clickImageButton))
+        self.proof_Image.isUserInteractionEnabled = true
+        self.proof_Image.addGestureRecognizer(tapUploadImage)
+    }
+    
+    //MARK:- Delgate
+    
+    func pickerResponse(userImage: UIImage, imageData: Any) {
+        print("get image \(userImage)   \(imageData)")
+        self.proof_Image.image = userImage
+        self.imageData = imageData as? NSData
+    }
+    
+    //MARK:- user defined function
+    
+    @objc func clickImageButton(){
+        imagePicker = ImagePiker(screen: self)
+        imagePicker.delegate = self
+        imagePicker.openPicker(pickerOpenType: "uploadProfile")
     }
     
     //MARK:- Actions
-
-    @IBAction func click_NextBttn(_ sender: Any) {
-        self.pushToSpaceSelectController()
+    
+    @IBAction func click_UploadButton(_ sender: Any) {
+        self.clickImageButton()
     }
     
+    @IBAction func click_NextBttn(_ sender: Any) {
+        Singelton.sharedInstance.addStorageModal.storageImage = self.imageData
+        self.pushToSpaceSelectController()
+    }
 }
